5c74e1951c8166fa83bbe7c65752b770d0c381d4
Roman <roma.k.v@mail.ru>
2022-03-15 23:40:28 +0600

    The second lesson task if

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+ 
 let numberOfFilms = +prompt('How match movies are you watvhed?', '');
 
 let personalMoviesDB = {
@@ -10,12 +10,29 @@ let personalMoviesDB = {
     privat: false
 };
 
-let a = prompt('Waht the lest muvie are you wathced?', ''),
-    b = prompt('What grade are you giv them?', ''),
-    c = prompt('Waht the lest muvie are you wathced?', ''),
-    d = prompt('What grade are you giv them?', '');
-    
-personalMoviesDB.movies[a] = b;
-personalMoviesDB.movies[c] = d;
+let a, b;
+
+for(let i = 0; i < 2; i++){
+    a = prompt('Waht the lest muvie are you wathced?', '');
+    b = prompt('What grade are you giv them?', '');
+  
+    if(a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50){
+        personalMoviesDB.movies[a] = b;
+        console.log('Corect');
+    }else{
+        console.log('Error');
+        i--;
+    }
+}
+
+if (personalMoviesDB.count < 10){
+    alert("Very small");
+}else if(personalMoviesDB.count > 10 && personalMoviesDB.count < 30){
+    alert("You are simple user");
+}else if(personalMoviesDB.count >= 30){
+    alert("You are awesome");
+}else{
+    alert("Error");
+}
 
 console.log(personalMoviesDB);
