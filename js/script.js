11c2539d24c411e9bb5e38226cee673eba22a962
Roman <roma.k.v@mail.ru>
2022-03-15 22:29:08 +0600

    the first app in lesson

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 "use strict";
 
-let incr = 5,
-    decr = 10;
+let numberOfFilms = +prompt('How match movies are you watvhed?', '');
 
-// ++incr;
-// --decr;
+let personalMoviesDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-console.log(++incr);
-console.log(--decr);
+let a = prompt('Waht the lest muvie are you wathced?', ''),
+    b = prompt('What grade are you giv them?', ''),
+    c = prompt('Waht the lest muvie are you wathced?', ''),
+    d = prompt('What grade are you giv them?', '');
+    
+personalMoviesDB.movies[a] = b;
+personalMoviesDB.movies[c] = d;
+
+console.log(personalMoviesDB);
