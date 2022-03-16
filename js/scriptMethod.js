4d7a526dc334e52525b0e3de8a24328d807ad78f
Roman <roma.k.v@mail.ru>
2022-03-16 22:53:24 +0600

    Method and weais

---
+++ js/scriptMethod.js
@@ -0,0 +1,29 @@
+"use strict";
+ let str = "hello";
+ let arr = [1, 2, 3, 4];
+
+//  console.log(arr.length);
+
+console.log(str.toUpperCase());
+console.log(str);
+console.log(str.toLowerCase());
+
+const fruit = 'Some fruite';
+
+console.log(fruit.indexOf('f'));
+
+const logg = 'Hello World';
+
+console.log(logg.slice(6, 11));
+
+console.log(logg.substring(0, 5));
+
+console.log(logg.substr(3, 6));
+
+const number = 12.2;
+
+console.log(Math.round(number));
+
+const number1 = '12.2';
+
+console.log(parseInt(number1));
