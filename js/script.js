c5ba6f322c98d9c593101fbdebf24b539b82b3af
Roman <roma.k.v@mail.ru>
2022-03-16 23:35:40 +0600

    The 3 practice chapter

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 "use strict";
  
-let numberOfFilms = +prompt('How match movies are you watvhed?', '');
+let numberOfFilms;
+
+function start(){
+    numberOfFilms = +prompt('How match movies are you watvhed?', '');
+
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt('How match movies are you watvhed?', '');
+    }
+}
+
+start();
 
 let personalMoviesDB = {
     count: numberOfFilms,
@@ -10,29 +20,54 @@ let personalMoviesDB = {
     privat: false
 };
 
-let a, b;
+function rememberUderAnswers(){
+    for(let i = 0; i < 2; i++){
+        let a = prompt('Waht the lest muvie are you wathced?', ''),
+            b = prompt('What grade are you giv them?', '');
+       
+         if(a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50){
+             personalMoviesDB.movies[a] = b;
+             console.log('Corect');
+         }else{
+             console.log('Error');
+             i--;
+         }
+     } 
+}
 
-for(let i = 0; i < 2; i++){
-    a = prompt('Waht the lest muvie are you wathced?', '');
-    b = prompt('What grade are you giv them?', '');
-  
-    if(a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50){
-        personalMoviesDB.movies[a] = b;
-        console.log('Corect');
+// rememberUderAnswers();
+
+function datectPersonalLevel(){
+    if (personalMoviesDB.count < 10){
+        alert("Very small");
+    }else if(personalMoviesDB.count > 10 && personalMoviesDB.count < 30){
+        alert("You are simple user");
+    }else if(personalMoviesDB.count >= 30){
+        alert("You are awesome");
     }else{
-        console.log('Error');
-        i--;
+        alert("Error");
+    }
+    
+      
+}
+
+// datectPersonalLevel();
+
+// console.log(personalMoviesDB);
+
+function writeYourGenres(){
+    for(let i = 1; i <= 3; i++){
+       const genres =  prompt(`What number of your favorite genre? ${i}`, '');
+       personalMoviesDB.genres[i - 1] = genres;
     }
 }
 
-if (personalMoviesDB.count < 10){
-    alert("Very small");
-}else if(personalMoviesDB.count > 10 && personalMoviesDB.count < 30){
-    alert("You are simple user");
-}else if(personalMoviesDB.count >= 30){
-    alert("You are awesome");
-}else{
-    alert("Error");
+writeYourGenres();
+
+function showMyDB(hiden){
+    if(!hiden){
+        console.log(personalMoviesDB);
+    }
 }
 
-console.log(personalMoviesDB);
+showMyDB(personalMoviesDB.privat);
